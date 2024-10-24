ec573e73e272fd1776226a7b9efac4e3a4ac3db2
Saad UR Rehman <you@example.com>
2024-10-24 20:31:09 +0500

    Added timestamps in remaining models

--- models/email-stats.model.js
+++ models/email-stats.model.js
@@ -31,7 +31,7 @@ const schema = new mongoose.Schema(
       required: true,
     },
   },
-  { strict: false, timestamps: true }
+  { timestamps: true, strict: false, timestamps: true }
 );
 
 const model = mongoose.model(EMAIL_STATS, schema);
