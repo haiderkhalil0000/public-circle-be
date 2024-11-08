5672a5b088c61f1509958746a472251e50085a85
Saad UR Rehman <you@example.com>
2024-11-08 22:36:47 +0500

    Added CRUD for assets

---
+++ models/assets.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Schema.Types;
+const {
+  constants: {
+    MODELS: { ASSET, COMPANY },
+    ASSETS_STATUS,
+  },
+} = require("../utils");
+
+const schema = new mongoose.Schema(
+  {
+    company: {
+      type: ObjectId,
+      required: true,
+      index: true,
+      ref: COMPANY,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(ASSETS_STATUS),
+      default: ASSETS_STATUS.ACTIVE,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.model(ASSET, schema);
+
+module.exports = model;
