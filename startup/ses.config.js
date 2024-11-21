6f4fde77949cbb29fbfd20204cc1a0adaa69c21b
Saad UR Rehman <you@example.com>
2024-11-21 15:07:11 +0500

    fix

--- startup/ses.config.js
+++ startup/ses.config.js
@@ -3,7 +3,7 @@ const { SESClient } = require("@aws-sdk/client-ses");
 const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
 
 const sesClient = new SESClient({
-  region: "ca-central-1",
+  region: "us-east-1",
   credentials: {
     accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
