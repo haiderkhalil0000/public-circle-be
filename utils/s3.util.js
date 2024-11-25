277702b4ae5ba615eb7f0ca70ea4ccc221e4a23b
Saad UR Rehman <you@example.com>
2024-11-25 05:23:08 +0500

    fix

--- utils/s3.util.js
+++ utils/s3.util.js
@@ -1,5 +1,8 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const {
+  S3Client,
+  PutObjectCommand,
+  getSignedUrl,
+} = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({ region: "us-east-1" });
 
