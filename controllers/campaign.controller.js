ab65d0af6100f9d15fe3b0b7ce3d187a65233d5f
Saad UR Rehman <saad.rehman@venndii.com>
2025-01-10 19:29:12 +0500

    fix

--- controllers/campaign.controller.js
+++ controllers/campaign.controller.js
@@ -91,8 +91,6 @@ const createCampaign = async ({
     basicUtil.validateObjectId({ inputString: segmentId });
   }
 
-  await validateCampaign({ campaign });
-
   const campaign = await Campaign.create({
     company: companyId,
     segments: segmentIds,
@@ -106,6 +104,7 @@ const createCampaign = async ({
   });
 
   if (runMode === RUN_MODE.INSTANT) {
+    await validateCampaign({ campaign });
     await runCampaign({ campaign });
   }
 };
